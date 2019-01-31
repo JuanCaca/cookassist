c859b530ebfb3b1d63103e5eb4b07a3a21c1badf
JuanCaca <47220144+JuanCaca@users.noreply.github.com>
2019-01-31 11:12:29 -0500

    Update detallePedido.py

--- detallePedido.py
+++ detallePedido.py
@@ -1,13 +1,36 @@
+class Detalle_pedido():
+	def __init__(self,cantidad,pedido,receta,producto):
+		"""Atributos:
+		self.cantidad
+		self.pedido
+		self.receta
+		self.producto
+		"""
+		self.setCantidad(cantidad)
+		self.setPedido(pedido)
+		self.setReceta(receta) 
+		self.setProducto(producto)
 
+	def setCantidad(self,cantidad):
+		self.cantidad = cantidad
 
-class DetallePedido:
+	def getCantidad(self):
+		return self.cantidad
 
-    def __init__(self):
-        '''ATTRIBUTES
-            self._cantidad
-            self._pedido
-            self._receta
-            self._producto
-        '''
+	def setPedido(self,pedido):
+		self.pedido = pedido
 
-        
+	def getPedido(self):
+		return self.pedido
+
+	def setReceta(self,receta):
+		self.receta = receta
+
+	def getReceta(self):
+		return self.receta
+
+	def setProducto(self,producto):
+		self.producto = producto
+
+	def getProducto(self):
+		return self.producto
